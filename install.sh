514ef32beedac0689b542555be15ea40d426bb34
deathrangerr <53964374+deathrangerr@users.noreply.github.com>
2020-12-22 11:52:07 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,22 +5,16 @@ sudo apt update
 sudo apt install wget git vim vi nano curl openjdk-8-jdk -y
 
 
-sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add –
+sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 
 
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
 
-sudo apt update
+apt update && apt-get install jenkins -y
 
 
-sudo apt install jenkins -y
-
-
-sudo systemctl start jenkins
-
-
-sudo systemctl status jenkins
+sudo systemctl start jenkins && sudo systemctl enable jenkins
 
 
 sudo usermod -aG sudo jenkins
@@ -28,5 +22,4 @@ sudo usermod -aG sudo jenkins
 
 sudo sed -i '26 a jenkins  ALL=(ALL) NOPASSWD:ALL' /etc/sudoers
 
-sed -i '63d HTTP_PORT=8090' /etc/default/jenkins
-sed -i '63 a HTTP_PORT=8090' /etc/default/jenkins
+sed -i '63d HTTP_PORT=8090' /etc/default/jenkins && sed -i '63 a HTTP_PORT=8090' /etc/default/jenkins
